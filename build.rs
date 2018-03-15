5b04e45b070e92dc9a1c3b630434fc0960c79e8a
Sam Vevang <sam.vevang@gmail.com>
2018-03-15 07:35:03 -0500

    use cmake crate to build proj.4 as submodule

--- build.rs
+++ build.rs
@@ -1,12 +1,39 @@
 extern crate bindgen;
+extern crate cmake;
 
+use std::fs::File;
+use std::io::Write;
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn main() {
+    if !Path::new("proj.4/.git").exists() {
+        let _ = Command::new("git").args(&["submodule", "update", "--init"])
+            .status();
+    }
+    let mut cfg = cmake::Config::new("proj.4");
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let proj_lib_dir = Path::new(&out_dir).join("lib");
+
+    // blow away the rpath and use the absolute path the dylib in the linked artifact
+    cfg.define("CMAKE_INSTALL_NAME_DIR", "${CMAKE_INSTALL_PREFIX}/lib");
+
+    cfg.build();
+
     // Tell cargo to tell rustc to link the system proj
     // shared library.
-    println!("cargo:rustc-link-lib=proj");
+    println!("cargo:rustc-link-lib=dylib=proj");
+    println!(
+        r"cargo:rustc-link-search={}",
+        proj_lib_dir.to_str().unwrap()
+    );
+
+    // create the wrapper header
+    let wrapper_path = Path::new(&out_dir).join("include/wrapper.h");
+    let mut f = File::create(&wrapper_path).unwrap();
+    f.write_all(b"#include \"proj.h\"").unwrap();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -16,7 +43,7 @@ fn main() {
         .blacklist_type("max_align_t")
         // The input header we would like to generate
         // bindings for.
-        .header("wrapper.h")
+        .header(wrapper_path.to_str().unwrap())
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
